511e6d0c7b067a61f10d942a825de40e714f999c
Lownoble <50346437+Lownoble@users.noreply.github.com>
2019-05-21 22:35:28 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include "graphics.h"
 #include "extgraph.h"
 #include "genlib.h"
@@ -18,25 +17,88 @@
 
 #include "imgui.h"
 
-#define TETRIS_MENU
-/*#define DEMO_MENU
+#define DEMO_MENU
 #define DEMO_BUTTON
 #define DEMO_ROTATE
-#define DEMO_EDITBOX*/
+#define DEMO_EDITBOX
 
-static double winwidth, winheight;
+static double winwidth, winheight;   // 窗口尺寸
+static float  rot_angle = 0;         // 三角形旋转角度
+static int    enable_rotation = 1;   // 允许旋转
+static int    show_more_buttons = 0; // 显示更多按钮
 
-void Main(){
-SetWindowTitle("tetris wula!");
-InitGraphics();
-winwidth = GetWindowWidth();
-winheight = GetWindowHeight();
-registerCharEvent(CharEventProcess); // 字符
-registerKeyboardEvent(KeyboardEventProcess);// 键盘
-registerMouseEvent(MouseEventProcess);      // 鼠标
-registerTimerEvent(TimerEventProcess);      // 定时器
+// 清屏函数，provided in libgraphics
+void DisplayClear(void); 
+// 计时器启动函数，provided in libgraphics
+void startTimer(int id,int timeinterval);
+
+// 用户的显示函数
+void display(void); 
+
+// 用户的字符事件响应函数
+void CharEventProcess(char ch)
+{
+	uiGetChar(ch); // GUI字符输入
+	display(); //刷新显示
 }
-#if defined(TETRIS_MENU)
+
+// 用户的键盘事件响应函数
+void KeyboardEventProcess(int key, int event)
+{
+	uiGetKeyboard(key,event); // GUI获取键盘
+	display(); // 刷新显示
+}
+
+// 用户的鼠标事件响应函数
+void MouseEventProcess(int x, int y, int button, int event)
+{
+	uiGetMouse(x,y,button,event); //GUI获取鼠标
+	display(); // 刷新显示
+}
+
+// 旋转计时器
+#define MY_ROTATE_TIMER  1
+
+// 用户的计时器时间响应函数
+void TimerEventProcess(int timerID)
+{
+	if( timerID==MY_ROTATE_TIMER && enable_rotation ) 
+	{
+		rot_angle += 10; // 全局变量
+		display(); // 刷新显示
+	}
+}
+
+// 用户主程序入口
+// 仅初始化执行一次
+void Main() 
+{
+	// 初始化窗口和图形系统
+	SetWindowTitle("俄罗斯方块");
+	//SetWindowSize(10, 10); // 单位 - 英寸
+	//SetWindowSize(15, 10);
+	//SetWindowSize(10, 20);  // 如果屏幕尺寸不够，则按比例缩小
+    InitGraphics();
+
+	// 获得窗口尺寸
+    winwidth = GetWindowWidth();
+    winheight = GetWindowHeight();
+
+	// 注册时间响应函数
+	registerCharEvent(CharEventProcess); // 字符
+	registerKeyboardEvent(KeyboardEventProcess);// 键盘
+	registerMouseEvent(MouseEventProcess);      // 鼠标
+	registerTimerEvent(TimerEventProcess);      // 定时器
+
+	// 开启定时器
+	startTimer(MY_ROTATE_TIMER, 50);            
+
+	// 打开控制台，方便输出变量信息，便于调试
+	// InitConsole(); 
+
+}
+
+#if defined(DEMO_MENU)
 // 菜单演示程序
 void drawMenu()
 { 
@@ -83,18 +145,61 @@ void drawMenu()
 	if( selection>0 ) selectedLabel = menuListHelp[selection];
 	if( selection==1 )
 		show_more_buttons = ! show_more_buttons;
-
-	// 显示菜单运行结果
-	x = winwidth/15;
-	y = winheight/8*7;
-	SetPenColor("Blue");
-	drawLabel(x,    y-=h, "Most recently selected menu is:");
-	SetPenColor("Red");
-	drawLabel(x+xindent, y-=h, selectedLabel);
-	SetPenColor("Blue");
-	drawLabel(x,    y-=h, "Control Variable Status");
-	SetPenColor("Red");
-	drawLabel(x+xindent, y-=h, enable_rotation ? "Rotation Enabled" : "Rotation Disabled");
-	drawLabel(x+xindent, y-=h, show_more_buttons ? "More Buttons" : "Less Button");
-}
+	}
 #endif // #if defined(DEMO_MENU)
+
+#if defined(DEMO_BUTTON)
+// 按钮演示程序
+void drawButtons()
+{
+	double fH = GetFontHeight();
+	double h = fH*2;  // 控件高度
+	double x = winwidth/2.5;  
+	double y = winheight/2+3*h; 
+	double w = winwidth/5; // 控件宽度
+	
+	if( button(GenUIID(0), x, y, w, h, "新游戏") )   //新游戏按钮 
+		{	
+			  
+			   
+		}
+		
+	if( button(GenUIID(0), x, y-3*h, w, h, "读取存档") )  //读取存档按钮 
+		{	
+			   
+			   
+		}
+		
+	if( button(GenUIID(0), x, y-6*h, w, h, "排行榜") )  //排行榜按钮 
+		{	
+			   
+			   
+		}
+		
+	if( button(GenUIID(0), x, y-9*h, w, h, "退出") )  //退出游戏 
+		{	
+			exit(-1); 	   
+		}
+}
+#endif
+
+void display()
+{
+	// 清屏
+	DisplayClear();
+
+#if defined(DEMO_MENU)
+	// 绘制和处理菜单
+	drawMenu();
+#endif
+
+
+#if defined(DEMO_BUTTON)
+	// 按钮
+	drawButtons();
+#endif
+
+
+	// 将绘制的结果put到屏幕上
+	//UpdateDisplay();
+}
