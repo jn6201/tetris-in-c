20f29e4d377e1a91c7414e982cd90bccc1d596ae
jn6201 <42483715+jn6201@users.noreply.github.com>
2019-05-28 23:02:18 +0800

    Update gamepart.c

--- gamepart.c
+++ gamepart.c
@@ -1,36 +1,19 @@
-#include "graphics.h"
-#include "extgraph.h"
-#include "genlib.h"
-#include "simpio.h"
-#include "conio.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include<time.h>
+#include"main.h"
 
-#include <windows.h>
-#include <olectl.h>
-#include <mmsystem.h>
-#include <wingdi.h>
-#include <ole2.h>
-#include <ocidl.h>
-#include <winuser.h>
-
-#define TIMER  1     /*500ms¶¨Ê±Æ÷ÊÂ¼þ±êÖ¾ºÅ*/
+#define TIMER  1     
 
 #define xiangsu 0.2
 int block[12][22];//初始化为0代表空白，，1，2，3，4，5，6，7，代表不同颜色
 int newblock;//用于存储下一个方块的a类型
 int indexx,indexy;//储存当前指示格
 int indexa,indexb;//储存当前指示类型
-int ms;
+int ms,score;
 
 
-void KeyboardEventProcess(int key, int event);
-void TimerEventProcess(int timerID);/*¶¨Ê±Æ÷ÏûÏ¢»Øµ÷º¯Êý*/
 void prefunction(){
-	for(int i=0;i<12;i++){
-		for(int j=0;j<22;j++){
+	int i,j;
+	for(i=0;i<12;i++){
+		for(j=0;j<22;j++){
 			if(i==0||i==11||j==0||j==21)block[i][j]=-10;
 		}
 	}
@@ -112,8 +95,9 @@ int isblock(int a,int b,int x,int y){
 	return 1;
 }
 void clean(){
-	for(int i=0;i<12;i++){
-		for(int j=0;j<22;j++){
+	int i,j;
+	for(i=0;i<12;i++){
+		for(j=0;j<22;j++){
 			if(block[i][j]>0)block[i][j]=0;
 		}
 	}
@@ -177,22 +161,25 @@ void drawblock(int a,int b,int x,int y){//前提是isblock=1
 
 int BlockInit(){
 	srand((int)time(0)); 
-	int t=int(rand())%7+1;
+	int t;
+	t=((int)rand())%7+1;
 	return t;
 }
 void blockicy(){
-	for(int i=0;i<11;i++){
-		for(int j=0;j<21;j++){
+	int i,j;
+	for(i=0;i<11;i++){
+		for(j=0;j<21;j++){
 			if(block[i][j]>0)block[i][j]=-block[i][j];
 		}
 	}
 }
 void blcokdecline(){
-int falg=1,i,j,m,n;
+int flag=1,i,j,m,n;
 for(j=1;j<21;j++){
-	falg=1;
+	flag=1;
 	for(i=1;i<11;i++)if(block[i][j]==0)flag=0;
 	if(flag==1){
+		score++;
 		for(m=j;m<21;m++){
 			for(n=1;n<11;n++){
 				block[n][m]=block[n][m+1];
@@ -202,7 +189,6 @@ for(j=1;j<21;j++){
 	}
 }
 void finaldraw(){
-	DisplayClear();
 	int i,j;
 	for(i=0;i<12;i++){
 		for(j=0;j<22;j++){
@@ -280,32 +266,28 @@ void finaldraw(){
 							
 		}
 }
-void display(){
-	DisplayClear();
-	if(isblock(indexa,indexb,indexx,indexy)==1){
-		clean();
-		drawblock(indexa,indexb,indexx,indexy);
-	}
-	if(isblock(indexa,indexb,index))
-}
+
 void MAINGAME(){
 	score=0;
+	isgame=1;
 	prefunction();
-	index=6;
+	indexx=6;
 	indexy=19;
 	newblock=BlockInit();
 	drawblock(BlockInit(),0,indexx,indexy);
-	finaldraw();
-	registerKeyboardEvent(KeyboardEventProcess);/*×¢²á¼üÅÌÏûÏ¢»Øµ÷º¯Êý*/
-	registerTimerEvent(TimerEventProcess);/*×¢²á¶¨Ê±Æ÷ÏûÏ¢»Øµ÷º¯Êý*/
+	display();
+	registerKeyboardEvent(KeyboardEventProcess);/*×￠2á?ü?ì???￠??μ÷oˉêy*/
+	registerTimerEvent(TimerEventProcess);/*×￠2á?¨ê±?÷???￠??μ÷oˉêy*/
 	ms=500;//最初速度
-	startTimer(TIMER, ms);/*500ms¶¨Ê±Æ÷´¥·¢*/
+	startTimer(TIMER, ms);/*500ms?¨ê±?÷′￥·￠*/
     
 } 
 
-void KeyboardEventProcess(int key, int event)
+void KeyboardEventProcess1(int key, int event)
 {
-	uiGetKeyboard(key,event); // GUI»ñÈ¡¼üÅÌ
+	int tmp;
+	tmp=ms;
+	uiGetKeyboard(key,event); // GUI??è??ü?ì
 		switch (event) {
 	 	case KEY_DOWN:
 			 switch (key) {
@@ -314,18 +296,18 @@ void KeyboardEventProcess(int key, int event)
 			 			clean();
 			 			indexb=(indexb+1)%4;
 			 			drawblock(indexa,indexb,indexx,indexy);
-			 			finaldraw();
+			 			display();
 			 		}
                      break;
 			     case VK_DOWN:
-
+					 ms=200;
                      break;
 			     case VK_LEFT:
 			         if(isblock(indexa,indexb,indexx-1,indexy)==1){
 			         	clean();
 			         	indexx--;
 			         	drawblock(indexa,indexb,indexx,indexy);
-			         	finaldraw();
+			         	display();
 			         }
                      break;
 			     case VK_RIGHT:
@@ -333,26 +315,29 @@ void KeyboardEventProcess(int key, int event)
 			         	clean();
 			         	indexx++;
 			         	drawblock(indexa,indexb,indexx,indexy);
-			         	finaldraw();
+			         	display();
 			         }
                      break;
                  default:break;
 			 }
 			 break;
 		case KEY_UP:
+			 ms=tmp;
 			 break;
 	 }	 
 }
 
 void TimerEventProcess(int timerID)
 {
-    switch(timerID){
+    if(isgame==1){
+    if(score%10==0&&ms>=300)ms=ms-50;
+	switch(timerID){
     	case TIMER:
     	if(isblock(indexa,indexb,indexx,indexy-1)==1){
     		indexy=indexy-1;
     		clean();
     		drawblock(indexa,indexb,indexx,indexy);
-    		finaldraw();
+    		display();
     	}
     	else{
     		blockicy();
@@ -365,150 +350,26 @@ void TimerEventProcess(int timerID)
     		indexy=19;
     		if(isblock(indexa,indexb,indexx,indexy)==1){
     			drawblock(indexa,indexb,indexx,indexy);
-    			finaldraw();
+    			display();
     		}
     		else{//开启游戏结束流程
     			cancelTimer(TIMER);
-
+				isgame=0;
     		}
     	}
 
-    }  
+    } 
+}
 	  
 }
-
-
-
-
-
-/*void DrawBlock(int a,int b,int x,int y){
-switch(a){
-	case 0: SetPenColor("Blue");
-			drawRectangle(x*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-			drawRectangle(x*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-			drawRectangle((x-1)*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-			drawRectangle((x-1)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-			break;
-	case 1: SetPenColor("Yellow");
-			switch(b): {
-				case 0:	drawRectangle(x*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle(x*xiangsu,(y-2)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						break;
-				case 1: drawRectangle(x*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x+1)*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle(x*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						break;
-					}
-			break;
-	case 2: SetPenColor("Red");
-			switch(b): {
-				case 0:	drawRectangle(x*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle(x*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y-2)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						break;
-				case 1: drawRectangle(x*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle(x*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x+1)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						break;
-					}
-	case 3: SetPenColor("Green");
-			switch(b): {
-				case 0:	drawRectangle(x*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle(x*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle(x*xiangsu,(y-2)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle(x*xiangsu,(y+1)*xiangsu,xiangsu,xiangsu,1);
-						break;
-				case 1: drawRectangle(x*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x+1)*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-2)*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						break;
-					}
-			break;
-	case 4: SetPenColor("Brown");
-			switch(b): {
-				case 0:	drawRectangle(x*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x+1)*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						break;
-				case 1: drawRectangle(x*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle(x*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle(x*xiangsu,(y-2)*xiangsu,xiangsu,xiangsu,1);
-						break;
-				case 2: drawRectangle(x*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle(x*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-2)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						break;
-				case 3: drawRectangle((x-1)*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle(x*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y+1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						break;			
-					}
-		break;
-		case 5: SetPenColor("Orange");
-			switch(b): {
-				case 0:	drawRectangle(x*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x+1)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						break;
-				case 1: drawRectangle(x*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle(x*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle(x*xiangsu,(y-2)*xiangsu,xiangsu,xiangsu,1);
-						break;
-				case 2: drawRectangle(x*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle(x*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-2)*xiangsu,(y1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y1)*xiangsu,xiangsu,xiangsu,1);
-						break;
-				case 3: drawRectangle((x-1)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle(x*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x)*xiangsu,(y)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x)*xiangsu,(y+1)*xiangsu,xiangsu,xiangsu,1);
-						break;			
-					}
-		break;
-		case 6: SetPenColor("Cyan");
-			switch(b): {
-				case 0:	drawRectangle(x*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x+1)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						break;
-				case 1: drawRectangle(x*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y+1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y-2)*xiangsu,xiangsu,xiangsu,1);
-						break;
-				case 2: drawRectangle(x*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-2)*xiangsu,(y)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						break;
-				case 3: drawRectangle((x)*xiangsu,y*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle(x*xiangsu,(y-1)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x-1)*xiangsu,(y)*xiangsu,xiangsu,xiangsu,1);
-						drawRectangle((x)*xiangsu,(y+1)*xiangsu,xiangsu,xiangsu,1);
-						break;			
-					}
-		break;					
-		}
+void pause(){
+	if(isgame==1)isgame=2;
 }
-void DrawMap(){
-	int i,j;
-	for(i=0;i<10;i++){
-		for(j=0;j<20;j++){
-			DrawBox(1+i*xiangsu,1+j*xiangsu,xiangsu,xiangsu);
-		}
-	}
-}*/
+void carryon(){
+	if(isgame==2)isgame=1;
+}
+
+
+
+
+
