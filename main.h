752459abb5c6e0f2862464e985385cf0d325f706
Lownoble <50346437+Lownoble@users.noreply.github.com>
2019-06-07 09:49:33 +0800

    Update main.h

--- main.h
+++ main.h
@@ -21,11 +21,15 @@
 #define DEMO_BUTTON
 #define DEMO_ROTATE
 #define DEMO_EDITBOX
+#define DEMO_TOOLS
 
-static double winwidth, winheight;   // 窗口尺寸
-static int    show_more_buttons = 0; // 显示更多按钮
-
-// 清屏函数，provided in libgraphics
+#define TIMER  1     
+#define xiangsu 0.2
+struct person{
+	char name[20];
+	int score;
+	char string[32];
+};
 void DisplayClear(void); 
 // 计时器启动函数，provided in libgraphics
 void startTimer(int id,int timeinterval);
@@ -34,25 +38,15 @@ void startTimer(int id,int timeinterval);
 void display(void); 
 
 // 用户的字符事件响应函数
-void CharEventProcess(char ch)
-{
-	uiGetChar(ch); // GUI字符输入
-	display(); //刷新显示
-}
+void CharEventProcess(char ch); 
 
 // 用户的键盘事件响应函数
-void KeyboardEventProcess(int key, int event)
-{
-	uiGetKeyboard(key,event); // GUI获取键盘
-	display(); // 刷新显示
-}
+void KeyboardEventProcess(int key, int event);
 
 // 用户的鼠标事件响应函数
-void MouseEventProcess(int x, int y, int button, int event)
-{
-	uiGetMouse(x,y,button,event); //GUI获取鼠标
-	display(); // 刷新显示
-}
+void MouseEventProcess(int x, int y, int button, int event);
+
+void TimerEventProcess(int timerID);
 
 //游戏操作函数 
 void Start_game();   //开始游戏 
@@ -66,7 +60,7 @@ void ShowHelp();//显示帮助
 void ShowHAbout();//显示关于 
 
 void KeyboardEventProcess1(int key, int event);
-void TimerEventProcess(int timerID);
+
 void prefunction();//预处理，每次刷新时第一步注册黑色边框
 void BlockMove(int direction);//把当前活动方块移动一格
 int isblock(int a,int b,int x,int y);//判断xy中心位置能否放下 一个ab型方块
@@ -78,10 +72,13 @@ void blcokdecline();//消灭满的行
 void finaldraw();//将数组形式最终编程图案
 void pause();//暂停 
 void carryon();//继续 
-int block[12][22];//初始化为0代表空白，，1，2，3，4，5，6，7，代表不同颜色
-int newblock;//用于存储下一个方块的a类型
-int indexx,indexy;//储存当前指示格
-int indexa,indexb;//储存当前指示类型
-int ms;//储存时间间隔
-int score;//
-int isgame;//0表示结束，1表示进行中，2表示暂停 
+void faster();//加速 
+void slower();//减速 
+
+enum gstates {
+	Welcome=0, Playing, Paused, GameEnd, Cleaning, changeUser, showBoard, showAbout, showHelp, Normalstate
+};
+void rankscore(struct person *p,int n);
+void memoryscore();
+bool MemoryArchiving();
+
